96bd762aaabe69e1c516f36eb62f96db5bb6ded7
rileyeh <rileyehatch@gmail.com>
2019-06-19 16:40:29 -0600

    inital commit

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+export default function budgetReducer(state = initialState, action) {
+    return state
+}
